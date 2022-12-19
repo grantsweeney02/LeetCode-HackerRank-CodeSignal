e53f2f47e2e700787c769ec7d053b60bee7e7da9
Grant Sweeney <grantsweeney02@gmail.com>
2022-12-19 16:10:07 -0500

    Create FirstBadVersion.java

---
+++ LeetCode/75DayPrep/Level1/FirstBadVersion.java
@@ -0,0 +1,19 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+      public class Solution extends VersionControl {
+        public int firstBadVersion(int n) {
+            int low = 1;
+            int high = n;
+            while(low < high) {
+                int middle = low + (high-low)/2;
+                if(isBadVersion(middle)) {
+                    return firstBadVersion(middle);
+                }
+                else {
+                    low = middle+1;
+                }
+            }
+            return low;
+        }
+    }
